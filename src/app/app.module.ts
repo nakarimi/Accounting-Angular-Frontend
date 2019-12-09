2e0a7da3981da787a8db7cff7e7161fb229080c6
karimi <n.karimi@pomtech.com>
2019-12-09 15:58:23 +0430

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './base/header/header.component';
+import { FooterComponent } from './base/footer/footer.component';
+import { NavigationComponent } from './base/navigation/navigation.component';
+import { ContentComponent } from './base/content/content.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    ContentComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
